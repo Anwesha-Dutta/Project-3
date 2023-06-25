9b14e911205de5d09491ec92fbb3d5fd8adda523
Anwesha-Dutta <anweshadutta891@gmail.com>
2023-06-25 20:25:23 +0530

    Updated Project 3 completed

--- main.js
+++ main.js
@@ -1,4 +1,11 @@
 function menu(){
    let menu = document.getElementById("menu");
    menu.classList.toggle("active");
+   cross();
+}
+
+function cross(){
+   let cross=document.getElementById("cross");
+   console.log("show cross after clicking");
+   cross.classList.toggle("active");
 }
